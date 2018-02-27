0b2dc7981b6992a5f0febafd616778708cab3df1
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2018-02-28 01:24:20 +0300

    End HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,9 +8,10 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -20,36 +21,43 @@ import java.util.stream.Collectors;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
+    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime,
+                                                                   LocalTime endTime, int caloriesPerDay) {
 
+        final Map<LocalDate, List<UserMeal>> groupMeals = mealList.stream()
+                .collect(Collectors.groupingBy(o -> o.getDateTime().toLocalDate()));
 
-        // Понять в какие даты был перебор по калориям.
-        // Перебрать всю еду, проставить флаги
+        final Set<UserMealWithExceed> mealsWithExceeds = new HashSet<>();
+        final List<LocalDate> dates = groupMeals.keySet().stream().collect(Collectors.toList());
 
-        mealList.stream().collect(Collectors.groupingBy(o -> o.getDateTime().toLocalDate()));
+        for (LocalDate date : dates) {
+            final List<UserMeal> userMeals = groupMeals.get(date);
+            final int caloriesInDay = userMeals.stream().mapToInt(UserMeal::getCalories).sum();
+            final boolean isExceed = caloriesInDay > caloriesPerDay;
 
+            final List<UserMealWithExceed> mealWithExceedList = userMeals.stream()
+                    .map(userMeal -> new UserMealWithExceed(userMeal.getDateTime(),
+                            userMeal.getDescription(),
+                            userMeal.getCalories(),
+                            isExceed))
+                    .collect(Collectors.toList());
+            mealsWithExceeds.addAll(mealWithExceedList);
+        }
 
-
-
-//        mealList.stream()
-//                .filter(userMeal -> TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime))
-
-
-
-
-        return null;
+        return mealsWithExceeds.stream()
+                .filter(mealWithExceed -> TimeUtil.isBetween(mealWithExceed.getDateTime().toLocalTime(), startTime, endTime))
+                .collect(Collectors.toList());
     }
 }
