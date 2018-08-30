77d881c504bf2723cd3587aca6ba6815fd71f4a7
Dmitriy Ovcharenko <ovcharenkodmitry@gmail.com>
2018-08-31 00:58:27 +0300

    Delete servlets and move functionality to JspMealController

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web.meal;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.javawebinar.topjava.service.MealService;
+
+@Controller
+public class JspMealController extends AbstractMealRestController {
+
+    private static final String MEALS = "meals";
+
+    @Autowired
+    MealService mealService;
+
+    @GetMapping("/meals")
+    public String getMeals(Model model) {
+        model.addAttribute(MEALS, super.getAll());
+        return MEALS;
+    }
+}
--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -7,10 +7,8 @@ import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
