769ae2631d6007c1e3e37406e3f79231bc218c71
Dmitriy Ovcharenko <ovcharenkodmitry@gmail.com>
2018-09-03 02:21:21 +0300

    Delete servlets and move functionality to JspMealController Create constants in Meal for parsing Add post "/meals" processing

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -1,20 +1,21 @@
 package ru.javawebinar.topjava.web.meal;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.service.MealService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 public class JspMealController extends AbstractMealRestController {
 
     private static final String MEALS = "meals";
-
-    @Autowired
-    MealService mealService;
+    private static final String REDIRECT = "redirect:";
 
     @GetMapping("/meals")
     public String getMeals(Model model) {
@@ -23,12 +24,27 @@ public class JspMealController extends AbstractMealRestController {
     }
 
     @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute("meal", new Meal());
+    public String fillModel(Model model) {
+        model.addAttribute("meal", new Meal(LocalDateTime.now(), "", 0));
         return "mealForm";
     }
 
+    @PostMapping("/meals")
+    public String create(HttpServletRequest request, Model model) {
+        final String id = request.getParameter("id");
+        final Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter(Meal.DATETIME)),
+                request.getParameter(Meal.DESCRIPTION),
+                Integer.valueOf(request.getParameter(Meal.CALORIES)));
 
+        if (StringUtils.isEmpty(id)) {
+            super.create(meal);
+        } else {
+            meal.setId(Integer.parseInt(id));
+            super.update(meal, Integer.parseInt(id));
+        }
+        return REDIRECT + MEALS;
+    }
 
 
 }
