684c6ba1425b4a6ea1664f2b6a698f5abc668604
Dmitriy Ovcharenko <ovcharenkodmitry@gmail.com>
2018-09-11 01:21:54 +0300

    fix JspMealController: constant and parsing int

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -17,23 +17,10 @@ import java.time.LocalTime;
 @Controller
 public class JspMealController extends AbstractMealRestController {
 
-    private static final String MEALS = "meals";
-    private static final String MEALS_URL = MEALS;
-    private static final String REDIRECT_MEALS = "redirect:" + MEALS;
-
-    private static final String ID = "id";
-    private static final String DATETIME = "dateTime";
-    private static final String DESCRIPTION = "description";
-    private static final String CALORIES = "calories";
-    private static final String START_DATE = "startDate";
-    private static final String END_DATE = "endDate";
-    private static final String START_TIME = "startTime";
-    private static final String END_TIME = "endTime";
-
     @GetMapping("/meals")
     public String getMeals(final Model model) {
-        model.addAttribute(MEALS, super.getAll());
-        return MEALS_URL;
+        model.addAttribute("meals", super.getAll());
+        return "meals";
     }
 
     @GetMapping("/create")
@@ -44,40 +31,41 @@ public class JspMealController extends AbstractMealRestController {
 
     @GetMapping("/update")
     public String fillModelUpdate(final HttpServletRequest request, final Model model) {
-        model.addAttribute("meal", super.get(Integer.valueOf(request.getParameter(ID))));
+        model.addAttribute("meal", super.get(Integer.valueOf(request.getParameter("id"))));
         return "mealForm";
     }
 
     @GetMapping("/delete")
     public String delete(final HttpServletRequest request) {
-        super.delete(Integer.valueOf(request.getParameter(ID)));
-        return REDIRECT_MEALS;
+        super.delete(Integer.valueOf(request.getParameter("id")));
+        return "redirect:meals";
     }
 
     @PostMapping("/meals")
     public String create(final HttpServletRequest request) {
-        final String id = request.getParameter(ID);
+        final String id = request.getParameter("id");
         final Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter(DATETIME)),
-                request.getParameter(DESCRIPTION),
-                Integer.valueOf(request.getParameter(CALORIES)));
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
 
         if (StringUtils.isEmpty(id)) {
             super.create(meal);
         } else {
-            meal.setId(Integer.parseInt(id));
-            super.update(meal, Integer.parseInt(id));
+            int intId = Integer.parseInt(id);
+            meal.setId(intId);
+            super.update(meal, intId);
         }
-        return REDIRECT_MEALS;
+        return "redirect:meals";
     }
 
     @PostMapping("/filter")
     public String getFiltered(final HttpServletRequest request, final Model model) {
-        final LocalDate startDate = DateTimeUtil.parseLocalDate(request.getParameter(START_DATE));
-        final LocalDate endDate = DateTimeUtil.parseLocalDate(request.getParameter(END_DATE));
-        final LocalTime startTime = DateTimeUtil.parseLocalTime(request.getParameter(START_TIME));
-        final LocalTime endTime = DateTimeUtil.parseLocalTime(request.getParameter(END_TIME));
-        model.addAttribute(MEALS, super.getBetween(startDate, startTime, endDate, endTime));
-        return MEALS_URL;
+        final LocalDate startDate = DateTimeUtil.parseLocalDate(request.getParameter("startDate"));
+        final LocalDate endDate = DateTimeUtil.parseLocalDate(request.getParameter("endDate"));
+        final LocalTime startTime = DateTimeUtil.parseLocalTime(request.getParameter("startTime"));
+        final LocalTime endTime = DateTimeUtil.parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
     }
 }
